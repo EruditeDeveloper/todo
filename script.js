696f77d4f2baafefb04fb52753c62635394d42a9
Erudite <20523951+EruditeDeveloper@users.noreply.github.com>
2023-11-28 23:48:19 +0000

    initial commit: 1

---
+++ script.js
@@ -0,0 +1,4 @@
+const div1 = document.querySelector(".todo-add-item");
+const div2 = document.querySelector(".todo-add-item");
+const div3 = document.querySelector(".todo-add-item");
+const div4 = document.querySelector(".todo-add-item");
